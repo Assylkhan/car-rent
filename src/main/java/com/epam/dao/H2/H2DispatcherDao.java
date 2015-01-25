5103b61768162abb6bb91a5a27568687d6123d1a
Assylkhan <assylkhan.rakhatov@mail.ru>
2015-01-25 23:58:51 +0600

    some corrections

--- src/main/java/com/epam/dao/H2/H2DispatcherDao.java
+++ src/main/java/com/epam/dao/H2/H2DispatcherDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class H2DispatcherDao implements DispatcherDao {
     private Connection connection = null;
     private static final String SELECT_BY_ID = "SELECT * FROM DISPATCHER WHERE ID = ?";
+    private static final String SELECT_BY_LOGIN = "SELECT * FROM DISPATCHER WHERE LOGIN = ?";
 
     public H2DispatcherDao(Connection connection) {
         this.connection = connection;
@@ -31,8 +32,30 @@ public class H2DispatcherDao implements DispatcherDao {
     }
 
     public Dispatcher findByLogin(String login) throws DaoException {
-
-        return null;
+        Dispatcher dispatcher = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.prepareCall(SELECT_BY_ID);
+            statement.setLong(1, id);
+            statement.executeQuery();
+            resultSet = statement.getResultSet();
+            if (resultSet.next()) {
+                dispatcher.setId(resultSet.getLong("ID"));
+                dispatcher.setLogin(resultSet.getString("login"));
+                dispatcher.setPassword(resultSet.getString("password"));
+                dispatcher.setFirstName(resultSet.getString("first_name"));
+                dispatcher.setLastName(resultSet.getString("last_name"));
+                dispatcher.setNationalId(resultSet.getInt("national_id"));
+                dispatcher.setPhone(resultSet.getString("phone"));
+            }
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        } finally {
+            try { resultSet.close(); } catch (SQLException e) {};
+            try { statement.close(); } catch (SQLException e) {};
+        }
+        return dispatcher;
     }
 
     @Override
